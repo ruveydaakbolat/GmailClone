5332cba89cde0a8443d5d7cc3fe8dbdc3b97d8d2
ruveydaakbolat <ruveydaakbolat3@gmail.com>
2023-09-24 18:26:52 +0300

    add mails

--- main.js
+++ main.js
@@ -1,17 +1,55 @@
-import { categories } from "./scripts/constants.js";
-import { renderCategories, ele } from "./scripts/ui.js";
+import { categories, mailData } from "./scripts/constants.js";
+import {
+  renderCategories,
+  ele,
+  renderMails,
+  toggleModal,
+} from "./scripts/ui.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   renderCategories(categories, "Gelen Kutusu");
+  renderMails(mailData);
 });
 
 ele.categoryList.addEventListener("click", (event) => {
+  const selected = event.target.dataset.id;
 
-    const selected = event.target.dataset.id;
-
-    renderCategories(categories, selected);
+  renderCategories(categories, selected);
 });
 
-ele.menu.addEventListener('click',(e) => {
-    ele.nav.classList.toggle('hide');
+ele.menu.addEventListener("click", (e) => {
+  ele.nav.classList.toggle("hide");
+});
+
+ele.createBtn.addEventListener("click", () => toggleModal(true));
+
+ele.closeBtn.addEventListener("click", () => toggleModal(false));
+
+ele.modalForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const reciever = e.target[1].value;
+  const title = e.target[2].value;
+  const message = e.target[3].value;
+
+  if (!reciever || !title || !message) {
+    alert("Lütfen bütün alanları doldurunuz...");
+  } else {
+    const newMail = {
+      id: new Date().getTime(),
+      sender: "Rüveyda",
+      reciever,
+      title,
+      message,
+      date: "23 Eylül",
+    };
+
+    mailData.unshift(newMail);
+
+    const strData = JSON.stringify(mailData);
+
+    localStorage.setItem("MAILS", strData);
+
+    renderMails(mailData);
+  }
 });
--- scripts/ui.js
+++ scripts/ui.js
@@ -1,11 +1,17 @@
 export const ele = {
-  categoryList: document.querySelector('.categories'),
-  menu: document.querySelector('#menu'),
-  nav: document.querySelector('nav')
-}
+  categoryList: document.querySelector(".categories"),
+  menu: document.querySelector("#menu"),
+  nav: document.querySelector("nav"),
+  mailsArea: document.querySelector(".mails"),
+  createBtn: document.querySelector(".create"),
+  closeBtn: document.querySelector('.close-modal'),
+  modal: document.querySelector(".modal-wrapper"),
+  modalForm: document.querySelector(".modal-wrapper form")
+  
+};
 
 export function renderCategories(data, active) {
-  ele.categoryList.innerHTML = '';
+  ele.categoryList.innerHTML = "";
   data.forEach((category) => {
     const categoryItem = document.createElement("li");
 
@@ -19,4 +25,31 @@ export function renderCategories(data, active) {
       `;
     ele.categoryList.appendChild(categoryItem);
   });
+}
+
+export function renderMails(mailData) {
+  if (!mailData) return;
+
+  const html = mailData.map(
+    (mail) => `
+  <div class="mail">
+    <div class="info">
+        <input type="checkbox" />
+        <i class="bi bi-star-fill"></i>
+        <b>${mail.sender}</b>
+    </div>
+    <div class="content">
+        <p class="title">${mail.title.slice(0, 30) + "..."}</p>
+        <p class="text">${mail.message.slice(0, 35) + "..."}</p>
+    </div>
+    <p class="time">${mail.date}</p>
+  </div>
+  `
+  );
+
+  ele.mailsArea.innerHTML = html.join(" ");
+}
+
+export function toggleModal(willOpen) {
+  ele.modal.style.display = willOpen ? "grid" : "none";
 }
